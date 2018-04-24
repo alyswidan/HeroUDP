b66bf95399eb02a1355e82e877ad7318c70fedb3
alyswidan <swidan12340@gmail.com>
2018-04-25 00:41:42 +0200

    added doc strings to sr_sender methods

--- sr_sender.py
+++ sr_sender.py
@@ -10,6 +10,13 @@ logger = get_stdout_logger()
 class SelectiveRepeatSender:
 
     def __init__(self, buffer_size=4, max_seq_num=-1):
+        """
+
+        :param buffer_size : the size of the window
+        :param max_seq_num:  maximum sequence number, if -1 or <2*buffer_size it is set to 2*window size
+        """
+
+
         self.max_seq_num = max(2*buffer_size, max_seq_num)
         self.current_seq_num = -1
         self.buffer = deque()
@@ -28,11 +35,21 @@ class SelectiveRepeatSender:
                 break
 
     def get_next_seq_num(self):
+        """
+        This gets the next sequence number modulo the max sequence number
+        :returns the next sequence number:
+        """
         self.current_seq_num += 1
         self.current_seq_num %= self.max_seq_num
         return self.current_seq_num
 
     def insert_in_buffer(self, data_chunk):
+        """
+        inserts the data chunk into the buffer if there's an empty slot
+        :param data_chunk: any of the types(int, string, byte-like)
+        :raises Value error if data_chunk is any other type
+        """
+
         if not isinstance(data_chunk, bytes):
             if isinstance(data_chunk, int):
                 data_chunk = str(data_chunk)
@@ -52,6 +69,11 @@ class SelectiveRepeatSender:
 
 
     def get_from_buffer(self):
+        """
+        gets a packet from the buffer if there's one, if the buffer is empty it blocks the
+        thread until an element becomes available
+        :return:
+        """
         self.buffer_full_sem.acquire()
         with self.buffer_lock:
             packet = self.buffer.popleft()
